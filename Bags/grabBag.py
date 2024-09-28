51b729acac82493557b7f4a7db39aeaf39366eb8
Statist <smemic97@gmail.com>
2024-09-27 21:14:25 -0400

    Finished Grabbag class

--- Bags/grabBag.py
+++ Bags/grabBag.py
@@ -1,3 +1,5 @@
+import random as random
+
 class grabBag():
     """Similar to the bag ADT, but the remove operation is replaced with grabItem(), which randomly removes an item from the bag"""
     
@@ -10,4 +12,41 @@ class grabBag():
         return len(self._elements)
     
     def contains(self, element: object) -> bool:
+        """Determines if a specific item exists in a bag"""
         return element in self._elements
+    
+    def add(self, element: object) -> None:
+        """Adds a bag to the container"""
+        self._elements.append(element)
+
+    def grabItem(self) -> object:
+        """Removes and returns an item at random"""
+        return self._elements.pop(random.randint(0, len(self._elements) - 1))
+    
+    def iter(self) -> object:
+        """Returns an iter for the object"""
+        return grabBagIterator(self._elements)
+    
+    def str(self) -> str:
+        """Displays the contents of the bag"""
+        return str(f"The Bag contains: {self._elements}")
+
+
+class grabBagIterator:
+    """Creates an iterator for the grabbag class"""
+    def __init__(self, element_list) -> None:
+        self._elements = element_list
+        self._counter = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self) -> object:
+        if self._counter < len(self._elements):
+            item = self._elements[self._counter]
+            self._counter += 1
+            return item
+        else:
+            raise StopIteration
+
+
